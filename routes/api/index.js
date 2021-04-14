183e3dc34598837754eeffbb7e37b3192d52d293
Ray Luna <leon.luna.ray@gmail.com>
2021-04-13 23:07:19 -0700

    user model and routing complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,8 @@
 // this is the middle part of the url/route
+const router = require('express').Router();
+const userRoutes = require('./users');
+
+// Book routes
+router.use('/users', userRoutes);
+
+module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
 const usersController = require('../../controllers/usersController');
+
+// Route matching /api/users
+router.route('/')
+    .post(usersController.create);
