0b1b9037ce6a1b80c1ca2d391a4c7ff520e4c0ef
Ray Luna <leon.luna.ray@gmail.com>
2021-04-14 23:28:47 -0700

    added find all method to controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,4 +8,13 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findAll: (req, res) => {
+    db.User
+      // query to list all users by username
+      // Need to find out if need to set req.query on the front end to say all
+      .find(req.query)
+      .sort({ username: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,7 @@
+const router = require('express').Router();
+const userRoutes = require('./users');
+
 // this is the middle part of the url/route
+router.use('/users', userRoutes);
+
+module.exports = router;
