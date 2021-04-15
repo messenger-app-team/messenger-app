ed307dd9b2b7087e8dee2b9ce4047e2de2f4f137
Ray Luna <leon.luna.ray@gmail.com>
2021-04-14 22:25:16 -0700

    restored tto previous commit app running

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,1 @@
 // this is the middle part of the url/route
-const router = require('express').Router();
-const userRoutes = require('./users');
-
-// Book routes
-router.use('/users', userRoutes);
-
-module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,2 @@
 const router = require('express').Router();
 const usersController = require('../../controllers/usersController');
-
-// Route matching /api/users
-router.route('/')
-    .post(usersController.create);
