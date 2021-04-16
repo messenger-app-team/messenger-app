11dd3e4389c438e59ff27a13303bd72c0dc7b2c5
Ray Luna <leon.luna.ray@gmail.com>
2021-04-15 22:26:37 -0700

    set up return statment

--- client/src/components/ChatApp.js
+++ client/src/components/ChatApp.js
@@ -3,3 +3,13 @@ import Avatar from './Avatar';
 import InputBox from './InputBox';
 import MsgArea from './MsgArea';
 import Send from './Send';
+
+function chatApp() {
+    return(
+        <>
+            <div className='message-window'>
+                
+            </div>
+        </>
+    )
+};
