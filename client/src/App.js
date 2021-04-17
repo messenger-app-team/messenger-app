97dea941947e86f292a6c5193f9a44798984b658
leon-luna-ray <leonluna.ray@gmail.com>
2021-04-17 15:44:12 -0700

    rm extra files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
-// import logo from './logo.svg';
-import './App.css';
 import Signup from "./components/Signup";
 import ChatApp from "./components/ChatApp/ChatApp";
-// import ChatApp from './components/ChatApp/ChatApp';
-import {AuthProvider} from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import "bootstrap/dist/css/bootstrap.min.css"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
