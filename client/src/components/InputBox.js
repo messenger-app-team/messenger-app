57fab84274e58d7e16ffb927cd07d09dcd671f1e
Ray Luna <leon.luna.ray@gmail.com>
2021-04-17 01:50:11 -0700

    import input box

--- client/src/components/InputBox.js
+++ client/src/components/InputBox.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 class InputBox extends Component {
   render() {
     return (
-      <div clasName='input-container'>
-        <input
-          id='msg-input'
-          className='msg-input'
-          type='text'
-          placehodler='Message...'
+      // <div clasName='input-container'>
+      //   <input
+      //     id='msg-input'
+      //     className='msg-input'
+      //     type='text'
+      //     placehodler='Message...'
+      //   />
+      // </div>
+
+       <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
         />
-      </div>
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Button</Button>
+        </InputGroup.Append>
+      </InputGroup>
     );
   }
 }
