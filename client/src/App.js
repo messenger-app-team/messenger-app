a6f96584971f8b847ad608dfb09bef402a08e86d
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2021-04-17 11:33:50 -0700

    Added Router functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
 import Signup from "./components/Signup";
-import ChatApp from './components/ChatApp/ChatApp';
+import ChatApp from "./components/ChatApp/ChatApp";
+// import ChatApp from './components/ChatApp/ChatApp';
 import {AuthProvider} from './contexts/AuthContext';
 import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     
-   
       <>
-      <ChatApp />
-
-      <AuthProvider>
-
-          <Signup/>;
-
-      </AuthProvider>
+      {/* <ChatApp /> */}
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/chatapp" component={ChatApp} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+     
      </>
   );
 }
