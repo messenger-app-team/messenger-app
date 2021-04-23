ac3f5048e7c1f4601e934e7ff62af235eee3d14b
Ray Luna <leon.luna.ray@gmail.com>
2021-04-22 21:34:10 -0700

    added nav to public page

--- client/src/components/PublicChat/PublicChat.js
+++ client/src/components/PublicChat/PublicChat.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import NavBar from '../NavBar';
 
 function PublicChat() {
-  return <div>Public Chat Page</div>;
+  // Added nav bar component
+  return (
+    <div className='public-chat'>
+      <NavBar />
+
+      <h1>Public Chat Page</h1>
+    </div>
+  );
 }
 
 export default PublicChat;
