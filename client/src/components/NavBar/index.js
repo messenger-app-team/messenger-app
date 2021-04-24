e26c4ff691196ba35523106ed9b78c88baa33a9c
Ray Luna <leon.luna.ray@gmail.com>
2021-04-24 12:58:37 -0700

    update

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -13,7 +13,7 @@ import capitalizeFirstLetter from "../../helpers";
 
 // build navbar to handle other utility functions.
 function NavBar({ currentUserName }) {
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [options, setOptions] = useState([]);
 
@@ -21,76 +21,77 @@ function NavBar({ currentUserName }) {
   const history = useHistory();
 
   const addUserToChat = (e, option) => {
-    db.ref("Contacts")
+    db.ref('Contacts')
       .child(currentUserName)
-      .once("value", async (snap) => {
+      .once('value', async snap => {
         if (snap && snap.val()) {
           const friends = Object.values(snap.val());
-          if (friends.filter((e) => option in e).length === 0) {
+          if (friends.filter(e => option in e).length === 0) {
             const makeObject = {};
             makeObject[option] = true;
-            await db.ref("Contacts").child(currentUserName).push(makeObject);
+            await db.ref('Contacts').child(currentUserName).push(makeObject);
             delete makeObject[option];
             makeObject[currentUserName] = true;
-            await db.ref("Contacts").child(option).push(makeObject);
+            await db.ref('Contacts').child(option).push(makeObject);
           }
         } else {
           const makeObject = {};
           makeObject[option] = true;
-          await db.ref("Contacts").child(currentUserName).push(makeObject);
+          await db.ref('Contacts').child(currentUserName).push(makeObject);
           delete makeObject[option];
           makeObject[currentUserName] = true;
-          await db.ref("Contacts").child(option).push(makeObject);
+          await db.ref('Contacts').child(option).push(makeObject);
         }
       });
-
   };
 
   async function handleLogout() {
-    setError("");
+    setError('');
     try {
       await logout();
-      history.push("/login");
+      history.push('/login');
     } catch {
-      setError("Failed to log out");
+      setError('Failed to log out');
     }
   }
 
-  const handleSearch = (query) => {
+  const handleSearch = query => {
     setIsLoading(true);
-    db.ref("users")
-      .orderByChild("userName")
+    db.ref('users')
+      .orderByChild('userName')
       .startAt(query.trim().toLowerCase())
-      .once("value", (snap) => {
+      .once('value', snap => {
         setOptions(Object.values(snap.val()));
         setIsLoading(false);
       });
   };
 
+  // Added username to the nav
   return (
     <>
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Messenger</Navbar.Brand>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Nav className="mr-auto">
-          <Nav.Link variant="link" onClick={handleLogout}>
+      <Navbar bg='primary' variant='dark'>
+        <Navbar.Brand href='#home'>Messenger</Navbar.Brand>
+        {error && <Alert variant='danger'>{error}</Alert>}
+        <Nav className='ml-2 mr-auto'>
+          <Nav.Link>{capitalizeFirstLetter(currentUserName)}</Nav.Link>
+          <Nav.Link variant='link' onClick={handleLogout}>
             Log Out
           </Nav.Link>
         </Nav>
         {/* <Form inline> */}
         <AsyncTypeahead
           filterBy={() => true}
-          id="async-example"
+          id='async-example'
           isLoading={isLoading}
-          labelKey="userName"
+          labelKey='userName'
           minLength={1}
           onSearch={handleSearch}
           options={options}
-          placeholder="Search for a user..."
+          placeholder='Search for a user...'
           renderMenuItemChildren={(option, props) => (
             <div
               key={option.uid}
-              onClick={(e) => {
+              onClick={e => {
                 addUserToChat(e, option.userName);
               }}
             >
@@ -98,9 +99,9 @@ function NavBar({ currentUserName }) {
                 alt={option.userName}
                 src={profileImage}
                 style={{
-                  height: "24px",
-                  marginRight: "10px",
-                  width: "24px",
+                  height: '24px',
+                  marginRight: '10px',
+                  width: '24px',
                 }}
               />
               <span>{option.userName}</span>
@@ -108,7 +109,7 @@ function NavBar({ currentUserName }) {
           )}
         />
         {/* </Form> */}
-        <span className="m-2">{capitalizeFirstLetter(currentUserName)}</span>
+        <span className='m-2'>{capitalizeFirstLetter(currentUserName)}</span>
       </Navbar>
     </>
   );
